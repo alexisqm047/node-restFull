495d42772a391b72a93b5426df5f53e8c8b596a3
alexisqm047 <alexisqm047@gmail.com>
2021-11-09 02:20:02 -0500

    crear servicio get

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const bodyparse = require("body-parser");
+const bodyparser = require("body-parser");
 const port = 4000;
 
 const server = new express();
+server.use(bodyparser.json);
+let respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: ''
+};
+
+server.get('/', function(request, response){
+    respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: 'Servicio git raiz no utilizar'
+    };
+    response.send(respuesta);
+});
 
 server.listen(port, () => {
     console.log("Servidor inciando -> " + port);
 });
+
